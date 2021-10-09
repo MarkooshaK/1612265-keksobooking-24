b5f6fcaf21250f7b95207f994bcb4b4c68c4df9e
Marina <marinakokh99@gmail.com>
2021-10-10 00:34:51 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -26,3 +26,51 @@ const getRandomFractionalNumber = (from, to, numberOfDecimalPlaces) => {
 
 getRandomIntegers(2, 3);
 getRandomFractionalNumber(3, 6, 6);
+
+const title = ['Лучшее предложение', 'Выгоднее быть не может', 'Дешевле некуда'];
+const type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkin = ['12:00', '13:00', '14:00'];
+const checkout = ['12:00', '13:00', '14:00'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const description = ['4 стены и окошко', 'Есть свет, но нет дверей', 'Удобная стальная мебель и резные плинтусы'];
+const photo = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const numberOfObject = 10;
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomIntegers(0, elements.length - 1)];
+};
+
+const getAdvertNearby = () => {
+  const result = [];
+  for (let i = 1; i <= numberOfObject; i++) {
+    const avatarId = i < 10 ? `0${i}` : i = 10;
+    const lat = getRandomFractionalNumber(35.65000, 35.70000, 5);
+    const lng =  getRandomFractionalNumber(139.70000, 139.80000, 5);
+    const amount = getRandomIntegers(1,7);
+    result.push ({
+      author: {
+        avatar: `img/avatars/user${avatarId}.png`,
+      },
+      offer: {
+        title: getRandomArrayElement(title),
+        address: `${lat}, ${lng}`,
+        price: getRandomIntegers(1000, 50000),
+        type: getRandomArrayElement(type),
+        rooms: getRandomIntegers(1, 20),
+        guests: getRandomIntegers(1, 10),
+        checkin: getRandomArrayElement(checkin),
+        checkout: getRandomArrayElement(checkout),
+        features: features.slice(0, amount),
+        description: getRandomArrayElement(description),
+        photo: photo.slice(0, amount),
+      },
+      location: {
+        lat,
+        lng,
+      },
+    });
+  }
+  return result;
+};
+
+const mockData = getAdvertNearby();
